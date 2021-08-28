359a7a9d3245fab016ab10022cd29b784c1a8fd0
jimanpark <im@gman.io>
2021-08-28 17:47:46 +0900

    Updates

--- _next/static/MpXYOhPA86q9fePpTNe3i/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/182-0749e5327cb79b94cfe2.js","static/chunks/pages/index-afff6e5e18b4d8ee53b0.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
